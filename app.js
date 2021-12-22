c20a835f98c0715aef0d74ff597804d840d814a5
Hammad Ali <34187587+hammadalibarejee@users.noreply.github.com>
2021-12-22 18:54:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,24 @@ app.use(bodyParser.json());
 app.use('/', router);
 // app.use('/users', router);
 app.all('*',(req,res,next)=>{
-    res.status(404).json({
-        status:"failed",
-        message:`Can't find ${req.originalUrl} on this server`
+//     res.status(404).json({
+//         status:"failed",
+//         message:`Can't find ${req.originalUrl} on this server`
+//     })
+    const err=new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status='fail';
+    err.statusCode=404;
+    next(err);
+
+})
+
+app.use((err,req,res,next)=>{
+    err.statusCode= err.statusCode || 500;
+    err.status=err.status || 'error'; 
+
+    err.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
     })
 })
 
