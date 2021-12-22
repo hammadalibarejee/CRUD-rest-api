b42dcb10002efe18cad36ac095f6b0895f6e6c57
Hammad Ali <34187587+hammadalibarejee@users.noreply.github.com>
2021-12-22 18:24:00 +0500

    Handling Unhandled routes
    Included debugging process with the help of ndb library and handled un handled routes

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/', router);
 // app.use('/users', router);
-
+app.all('*',(req,res,next)=>{
+    res.status(404).json({
+        status:"failed",
+        message:`Can't find ${req.originalUrl} on this server`
+    })
+})
 
 
 module.exports = app;
